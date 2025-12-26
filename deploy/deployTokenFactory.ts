aae28b366e3a339a2259a1c00a8f56b3b3123544
nathanwrites <gfsshqj45346@outlook.com>
2025-12-26 17:06:21 +0800

    chore(deploy): add factory deployment script

---
+++ deploy/deployTokenFactory.ts
@@ -0,0 +1,19 @@
+import { DeployFunction } from "hardhat-deploy/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployer } = await hre.getNamedAccounts();
+  const { deploy } = hre.deployments;
+
+  const deployedFactory = await deploy("ConfidentialTokenFactory", {
+    from: deployer,
+    log: true,
+  });
+
+  console.log(`ConfidentialTokenFactory contract: `, deployedFactory.address);
+};
+
+export default func;
+func.id = "deploy_tokenFactory";
+func.tags = ["ConfidentialTokenFactory"];
+
